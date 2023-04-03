19a76ad0f4daa05326f6a8930592397a9fc1dada
+ <+>
2023-04-03 16:34:05 +0300

    Done with Assignments

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+// Select the main node and remove it
+const main = document.querySelector('main#main');
+main.remove();
+
+// Create a new h1 node with id 'victory' and text content
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = "YOUR-NAME is the champion";
+
+// Append the new h1 node to the body of the document
+document.body.appendChild(newHeader);
